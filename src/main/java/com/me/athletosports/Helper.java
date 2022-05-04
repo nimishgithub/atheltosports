1027bc17ca32a0d48cdadaccb19f7cd168dbfb23
Nimish Sharma <sharma.nim@northeastern.edu>
2022-05-03 23:49:23 -0400

    JSP modified for admin signup

--- src/main/java/com/me/athletosports/Helper.java
+++ src/main/java/com/me/athletosports/Helper.java
@@ -1,6 +1,4 @@
 package com.me.athletosports;
-
-import javax.swing.*;
 import java.text.SimpleDateFormat;
 import java.sql.Date;
 
@@ -27,17 +25,7 @@ public class Helper {
         return simpleDateFormat.format(convertLongToDate(date));
     }
 
-    // close operation = JFrame.DISPOSE_ON_CLOSE
-    public static void wrapInWindow(JComponent comp, int closeOeration) {
-        JFrame frm = new JFrame( comp.getClass().getName() );
-        frm.getContentPane().add( comp );
-        frm.pack();
-        frm.setDefaultCloseOperation(closeOeration);
-        frm.setLocation(0, 0);
-        frm.setVisible(true);
-    }
-
-    public static Long get60MonthEpochOffset() {
+    public static Long getSixtyMonthEpochOffset() {
         return convertStringDateToLong("2020-01-01") - convertStringDateToLong("2015-01-01");
     }
 
@@ -45,7 +33,7 @@ public class Helper {
         return convertLongToFormattedString(epoch).substring(0, 4);
     }
     
-    public static Long get1MonthLong() {
+    public static Long getOneMonthLong() {
         return convertStringDateToLong("2020-02-01") - convertStringDateToLong("2020-01-01");
     }
 
--- src/main/java/com/me/athletosports/SessionInterceptor.java
+++ src/main/java/com/me/athletosports/SessionInterceptor.java
@@ -5,7 +5,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import javax.servlet.http.HttpSession;
 
 @Component
 public class SessionInterceptor implements HandlerInterceptor {
